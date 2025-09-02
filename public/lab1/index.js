1f25650b94a08605c76c927e64bfc7e5779f8d3c
root <root@webdevitmo-plida.yetti>
2025-09-02 16:23:02 +0000

    added event listeners for add to cart buttons

--- public/lab1/index.js
+++ public/lab1/index.js
@@ -1,5 +1,16 @@
-function addToCart(item) {
-	console.log("Added " + item)
+function addToCart(id, price){
+	if (id === undefined || price === undefined){
+		return;
+	}
+	console.log(id, price);
 }
 
 const cart = {};
+
+var productList = document.getElementById('product-list');
+productList.addEventListener('click', (event) => {
+	const isButton = event.target.nodeName === 'BUTTON';
+	if (isButton) {
+		addToCart(event.target.dataset.id, event.target.dataset.price);
+	}
+})
