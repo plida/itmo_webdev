6d87b7a9833a3188aaa9bc5651b90bb8766d8620
plida <Elisaveta.Krestyanova@yandex.ru>
2025-09-07 21:03:16 +0000

    refactoring: populateCartContents

--- public/lab1/script.js
+++ public/lab1/script.js
@@ -1,13 +1,13 @@
 const LOOKUP_TABLE = {
-	'1': { name: 'Apples', weight:'1 lb', price: 1.99, img: 'media/shelley-pauls-unsplash.jpg'},
-	'2': { name: 'Tangerines', weight:'1 lb', price: 1.59, img: 'media/erol-ahmed-unsplash.jpg'},
-	'3': { name: 'Bananas', weight:'1 lb', price: 0.56, img: 'media/ries-bosch-unsplash.jpg'},
-	'4': { name: 'Pineapples', weight:'1 lb', price: 0.99, img: 'media/phoenix-han-unsplash.jpg'},
-	'5': { name: 'Pears', weight:'1 lb', price: 2.19, img: 'media/maksim-shutov-unsplash.jpg'},
+  '1': { name: 'Apples', weight:'1 lb', price: 1.99, img: 'media/shelley-pauls-unsplash.jpg'},
+  '2': { name: 'Tangerines', weight:'1 lb', price: 1.59, img: 'media/erol-ahmed-unsplash.jpg'},
+  '3': { name: 'Bananas', weight:'1 lb', price: 0.56, img: 'media/ries-bosch-unsplash.jpg'},
+  '4': { name: 'Pineapples', weight:'1 lb', price: 0.99, img: 'media/phoenix-han-unsplash.jpg'},
+  '5': { name: 'Pears', weight:'1 lb', price: 2.19, img: 'media/maksim-shutov-unsplash.jpg'},
   '6': { name: 'Grape', weight:'1 oz', price: 0.19, img: 'media/alexander-schimmeck-unsplash.jpg'},
   '7': { name: 'Cherries', weight:'1 oz', price: 0.18, img: 'media/roksolana-zasiadko-unsplash.jpg'},
   '8': { name: 'Strawberries', weight:'1 oz', price: 0.19, img: 'media/massimiliano-martini-unsplash.jpg'},
-	'9': { name: 'Peaches', weight:'1 lb', price: 2.37, img: 'media/eric-prouzet-unsplash.jpg'},
+  '9': { name: 'Peaches', weight:'1 lb', price: 2.37, img: 'media/eric-prouzet-unsplash.jpg'},
 }
 
 let cart = [];
@@ -15,6 +15,8 @@ let cart = [];
 if (localStorage.getItem('cart')){
 	cart = JSON.parse(localStorage.getItem('cart'));
 }
+
+updateCart();
 
 function addToCart(id){
   if (id === undefined){
@@ -31,6 +33,7 @@ function addToCart(id){
   else{
     item.quantity++;
   }
+  updateCart();
 }
 
 function removeFromCart(id, removingFully = false){
@@ -44,15 +47,18 @@ function removeFromCart(id, removingFully = false){
 		cart.splice(n, 1);
 		item.quantity = 0;
   }
+  updateCart();
 }
 
 function emptyCart(){
   cart = [];
+  updateCart();
 }
 
 function updateCart(){
  localStorage.setItem('cart', JSON.stringify(cart)); 
  updateCartCountTotal();
+ populateCartContents();
 }
 
 function updateCartCountTotal(){
@@ -98,9 +104,9 @@ function updateCartCountTotal(){
     }
   }
   for (item of cart){
-    let addToCartBtn = document.querySelector('#add-to-cart-btn' + item.id);
-    let incrementBtn = document.querySelector('#increment-cart-item-btn' + item.id);
-    let decrementBtn = document.querySelector('#decrement-cart-item-btn' + item.id);
+    let addToCartBtn = document.querySelector('#item__add-btn' + item.id);
+    let incrementBtn = document.querySelector('#item__increment-btn' + item.id);
+    let decrementBtn = document.querySelector('#item__decrement-btn' + item.id);
     if (item.quantity > 99){
       if (addToCartBtn){
         addToCartBtn.disabled = true;
@@ -126,9 +132,101 @@ function updateCartCountTotal(){
       }
     }
   }
-  // TODO: item total count (sum * item.quantity)
-  //...
-  
+
+  for (const [key, value] of Object.entries(LOOKUP_TABLE)){
+    let itemCount = 0;
+    let itemTotal = 0;
+    
+    let item = cart.find(item => item.id === key); 
+    if (item){
+      itemCount = item.quantity;
+      itemTotal = value.price * item.quantity;
+      itemTotal = Math.round((itemTotal * 100) / 100);
+      itemTotal = itemTotal.toFixed(2);
+    }
+    
+    let menuCounts = document.querySelectorAll('#menu__count' + key);
+    for (let menuCount of menuCounts){
+     menuCount.innerText = '$' + itemCount;
+    }
+
+    let itemTotals = document.querySelectorAll('#item__total' + key);
+    for (let itemTotal of itemTotals){
+     itemTotal.innerText = '$' + itemTotal;
+    }
+  }
 }
 
-updateCart()
+function populateCartContents(){
+  let cartContents = document.querySelector('#cart-contents');
+  if (cartContents === null){
+    return;
+  }
+
+  let list = cartContents.querySelector('ul');
+  if (cart.length === 0){
+    list.innerHTML = 'Nothing here!';
+    return;
+  }
+  listClone = list.cloneNode(true);
+  listClone.innerHTML = '';
+  for (item of cart){
+    const LOOKUP_ITEM = LOOKUP_TABLE[item.id];
+
+    let listItem = document.createElement('li');
+      listItem.classList.add('cart-contents__item');
+
+    let image = document.createElement('img');
+      image.src = LOOKUP_ITEM.img;
+      image.alt = LOOKUP_ITEM.name;
+    let text = document.createElement('span');
+      text.classList.add('item__text');
+      text.textContent = LOOKUP_ITEM.name + ' ' + 
+      LOOKUP_ITEM.weight + ' ' + LOOKUP_ITEM.price;
+    let totalPrice = document.createElement('span');
+      totalPrice.classList.add('item__total');
+      totalPrice.id = 'item__total' + item.id;
+    
+    let menu = document.createElement('div');
+      menu.classList.add('item__menu');
+      menu.id = 'item__menu' + item.id;
+    let incrementBtn = document.createElement('button');
+      incrementBtn.classList.add('item__increment-btn');
+      incrementBtn.id = 'item__increment-btn' + item.id;
+    let decrementBtn = document.createElement('button');
+      decrementBtn.classList.add('item__decrement-btn');
+      decrementBtn.id = 'item__decrement-btn' + item.id;
+    let menuCount = document.createElement('span');
+      menuCount.id = 'menu__count' + item.id;
+    menu.append(decrementBtn);
+    menu.append(menuCount);
+    menu.append(incrementBtn);
+
+    let removeBtn = document.createElement('button');
+      removeBtn.classList.add('item__remove-btn');
+      removeBtn.id = 'item__remove-btn' + item.id;
+    
+    listItem.append(image);
+    listItem.append(text);
+    listItem.append(totalPrice);
+    listItem.append(menu);
+    listItem.append(removeBtn);
+
+    listClone.append(listItem);
+  }
+  listClone.addEventListener('click', (event) => {
+		const isButton = event.target.nodeName === 'BUTTON';
+		if (isButton && event.target.classList.contains('item__increment-btn')){
+			addToCart(event.target.id.slice(19));
+		}
+		if (isButton && event.target.classList.contains('item__decrement-btn')){
+			removeFromCart(event.target.id.slice(19));
+		}
+		if (isButton && event.target.classList.contains('item__remove-btn')){
+			removeFromCart(event.target.id.slice(16), true);
+		}
+	})
+
+  list.parentNode.appendChild(listClone);
+	list.remove();
+}
