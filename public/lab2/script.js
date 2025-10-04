bcc3405e53fecbf2bbbd1b0e23ad94d146b2ddd4
plida <Elisaveta.Krestyanova@yandex.ru>
2025-10-04 18:14:14 +0000

    header adjustments

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -16,6 +16,7 @@ HEADER_NAV.appendChild(HEADER_LIST);
 const HEADER_LOGO = document.createElement('li');
 const HEADER_LOGO_LINK = document.createElement('a');
 HEADER_LOGO_LINK.href = 'index.html';
+HEADER_LOGO_LINK.classList.add('disabled');
 HEADER_LOGO.appendChild(HEADER_LOGO_LINK)
 const HEADER_LOGO_IMAGE = document.createElement('img');
 HEADER_LOGO_IMAGE.src = 'media/3.png';
@@ -29,9 +30,12 @@ HEADER_LOGO_MIDDLE.classList.add('site-name-middle');
 HEADER_LOGO_LINK.appendChild(HEADER_LOGO_LONG);
 HEADER_LOGO_LINK.appendChild(HEADER_LOGO_MIDDLE);
 const HEADER_GIT = document.createElement('li');
+const HEADER_GIT_LINK = document.createElement('a');
+HEADER_GIT_LINK.href = 'https://github.com/plida/itmo_webdev/tree/main/public/lab2';
+HEADER_GIT.appendChild(HEADER_GIT_LINK);
 const HEADER_GIT_IMAGE = document.createElement('img');
 HEADER_GIT_IMAGE.src = 'media/Octicons-mark-github.svg.png';
-HEADER_GIT.appendChild(HEADER_GIT_IMAGE);
+HEADER_GIT_LINK.appendChild(HEADER_GIT_IMAGE);
 HEADER_LIST.appendChild(HEADER_LOGO);
 HEADER_LIST.appendChild(HEADER_GIT);
 
