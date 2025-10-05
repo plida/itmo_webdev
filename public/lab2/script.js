2e39d92955e6b8c93ce1dc051cf6281c05df8599
plida <Elisaveta.Krestyanova@yandex.ru>
2025-10-05 05:32:14 +0000

    can add and remove tasks

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,4 +1,3 @@
-
 // PAGE
 const PAGE_BODY = document.body;
 const PAGE_HEADER = document.createElement('HEADER');
@@ -46,26 +45,104 @@ PAGE_FOOTER_TEXT.textContent = 'Plida 2025';
 PAGE_FOOTER.append(PAGE_FOOTER_TEXT);
 
 // MAIN
-
-const STATUSES = ["incomplete", "in progress", "complete"];
-
-let taskList = [
-  { name: "1", description: "aaa", status: 0 },
-  { name: "2", description: "bbb", status: 0 },
-  { name: "3", description: "ccc", status: 0 },
-
-];
-
 const TASK_LIST = document.createElement('section');
 TASK_LIST.classList.add('task-list')
 const TASK_LIST_UL = document.createElement('ul');
 TASK_LIST.append(TASK_LIST_UL);
-for (task of taskList){
-  let taskElem = document.createElement('li');
-  taskElem.textContent = task.name + ' ' + task.description;
-  let taskStatus = document.createElement('span');
-  taskStatus.textContent = task.status;
-  taskElem.appendChild(taskStatus);
-  TASK_LIST_UL.appendChild(taskElem);
-}
 PAGE_MAIN.append(TASK_LIST);
+const TASK_FORM = document.createElement('form');
+const TASK_FORM_NAME = document.createElement('input');
+TASK_FORM_NAME.name = 'name';
+const TASK_FORM_DESCRIPTION = document.createElement('input');
+TASK_FORM_DESCRIPTION.name = 'description';
+const TASK_FORM_SUBMIT = document.createElement('input');
+TASK_FORM_SUBMIT.type = 'submit';
+TASK_FORM.appendChild(TASK_FORM_NAME);
+TASK_FORM.appendChild(TASK_FORM_DESCRIPTION);
+TASK_FORM.appendChild(TASK_FORM_SUBMIT);
+PAGE_MAIN.appendChild(TASK_FORM);
+
+const STATUSES = ['incomplete', 'in progress', 'complete'];
+
+let taskList = [
+  { id: 0, name: '1', description: 'aaa', status: 0 },
+  { id: 1, name: '2', description: 'bbb', status: 1 },
+  { id: 2, name: '3', description: 'ccc', status: 2 },
+];
+
+function compareID( a, b ) {
+  if ( a.id < b.id ){
+    return 1;
+  }
+  if ( a.id > b.id ){
+    return -1;
+  }
+  return 0;
+}
+
+
+function listTasks(){
+  let list_UL = TASK_LIST.querySelector('ul');
+  let list_UL_clone = list_UL.cloneNode(true)
+  list_UL_clone.textContent = '';
+  for (task of taskList){
+    let taskElem = document.createElement('li');
+    taskElem.textContent = task.name + ' ' + task.description;
+    let taskElemBtn = document.createElement('button');
+    taskElemBtn.classList.add('task__remove-btn');
+    taskElemBtn.id = 'task_remove-btn' + task.id;
+    taskElem.appendChild(taskElemBtn);
+    taskElemBtn.addEventListener('click', (event) => {
+		  removeTask(event.target.id.slice('task_remove-btn'.length));
+    })
+    let taskStatus = document.createElement('span');
+    taskStatus.textContent = STATUSES[task.status];
+    taskElem.appendChild(taskStatus);
+    list_UL_clone.appendChild(taskElem);
+  }
+  TASK_LIST.appendChild(list_UL_clone);
+  list_UL.remove();
+}
+
+function addTask(){
+  const data = new FormData(TASK_FORM);
+  let taskListClone = structuredClone(taskList);
+  taskListClone.sort(compareID);
+  let highestID = taskListClone[0].id;
+  taskList.push({
+    id: highestID + 1,
+    name: data.get('name'), 
+    description: data.get('description'),
+    status: 0
+  });
+  updateTasks();
+}
+
+function removeTask(id){
+  let item = taskList.find(item => item.id === parseInt(id));
+  if (item === undefined){
+    return;
+  } 
+  let n = taskList.indexOf(item);
+  taskList.splice(n, 1);
+  item.quantity = 0;
+  updateTasks();
+}
+
+function updateTasks(){
+  localStorage.setItem('tasklist', JSON.stringify(taskList)); 
+  listTasks();
+}
+
+TASK_FORM.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  addTask(event);
+});
+
+if (localStorage.getItem('tasklist')){
+	taskList = JSON.parse(localStorage.getItem('tasklist'));
+}
+
+updateTasks();
+
+
