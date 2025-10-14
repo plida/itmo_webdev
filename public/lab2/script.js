f5dd3336c7226b69cc5eba110eeb5c37611a46b2
plida <Elisaveta.Krestyanova@yandex.ru>
2025-10-14 05:06:32 +0000

    refactored main page DOM, task DOM set up

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,22 +1,27 @@
+// VARIABLES
 const STATUSES = ['', 'incomplete', 'in progress', 'complete'];
 
 let taskList = [
   { id: 0, name: '1', description: 'aaa', date: "2018-07-22", status: 1 },
   { id: 1, name: '2', description: 'bbb', date: "2019-07-22", status: 2 },
   { id: 2, name: '3', description: 'ccc', date: "2020-07-22", status: 3 },
-  { id: 3, name: '4', description: 'ccc', date: "2020-07-22", status: 3 },
-  { id: 4, name: '5', description: 'ccc', date: "2020-07-22", status: 3 },
+  { id: 3, name: '4', description: 'ccc', date: "2020-07-22", status: 1 },
+  { id: 4, name: '5', description: 'ccc', date: "2020-07-22", status: 2 },
   { id: 5, name: '6', description: 'ccc', date: "2020-07-22", status: 3 },
-  { id: 6, name: '7', description: 'ccc', date: "2020-07-22", status: 3 },
-  { id: 7, name: '8', description: 'ccc', date: "2020-07-22", status: 3 },
+  { id: 6, name: '7', description: 'ccc', date: "2020-07-22", status: 1 },
+  { id: 7, name: '8', description: 'ccc', date: "2020-07-22", status: 2 },
   { id: 8, name: '9', description: 'ccc', date: "2020-07-22", status: 3 },
-  { id: 9, name: '10', description: 'ccc', date: "2020-07-22", status: 3 },
+  { id: 9, name: '10', description: 'ccc', date: "2020-07-22", status: 1 },
 ];
 
 sorttype = "id";
 nameFilter = "";
 statusFilter = 1;
 
+
+
+
+// SORTING
 function compareID( a, b ) {
   if ( a.id > b.id ){
     return 1;
@@ -77,6 +82,63 @@ function compareStatusInv( a, b ) {
   return 0;
 }
 
+
+
+
+// TASKS
+function updateTasks(){
+  localStorage.setItem('tasklist-test', JSON.stringify(taskList)); 
+  listTasks();
+}
+
+
+function getSortedTaskList(sorttype){
+  let sortedTaskList = structuredClone(taskList);
+  switch (sorttype){
+    case "date":
+      sortedTaskList = sortedTaskList.sort(compareDate);
+      break;
+    case "dateinv":
+      sortedTaskList = sortedTaskList.sort(compareDateInv);
+      break;
+    case "id":
+      sortedTaskList = sortedTaskList.sort(compareID);
+      break;
+    case "idinv":
+      sortedTaskList = sortedTaskList.sort(compareIDInv);
+      break;
+    default:
+      sortedTaskList = sortedTaskList.sort(compareID);
+  }
+  return sortedTaskList;
+}
+
+
+function listTasks(){
+  let listUL = task_window.querySelector('ul');
+  let listUL_clone = listUL.cloneNode();
+  listUL_clone.textContent = '';
+  task_window.appendChild(listUL_clone);
+  listUL.remove();
+  let sortedTaskList = getSortedTaskList(sorttype);
+
+  for (task of sortedTaskList){
+    if (task.name.toUpperCase().indexOf(nameFilter) === -1) {
+      continue;
+    }
+    if (task.status > 0 && task.status != statusFilter){
+      continue;
+    }
+
+    let taskElem = document.createElement('li');
+    taskElem.setAttribute('index', task.id)
+    setTaskDOM(task, taskElem);
+    listUL_clone.appendChild(taskElem);
+  }
+}
+
+
+
 // PAGE SETUP
 const page = document.body;
 const header = document.createElement('header');
@@ -128,19 +190,221 @@ footer.appendChild(footer_text);
 const task_window = document.createElement('section');
 main.appendChild(task_window);
 
-const task_window_search = document.createElement('section');
-task_window.appendChild(task_window_search);
-const task_window_search_input = document.createElement('input');
-task_window_search.appendChild(task_window_search_input);
 
-const task_window_sort = document.createElement('section');
-task_window.appendChild(task_window_sort);
-const task_window_sort_date = document.createElement('button');
-task_window_sort_date.textContent = 'sort by date';
-task_window_sort.appendChild(task_window_sort_date);
-const task_window_sort_id = document.createElement('button');
-task_window_sort_id.textContent = 'sort by ID';
-task_window_sort.appendChild(task_window_sort_id);
+const task_search = document.createElement('section');
+task_window.appendChild(task_search);
+const task_search_input = document.createElement('input');
+task_search.appendChild(task_search_input);
 
-const task_window_filter = document.createElement('section');
-task_window.appendChild(task_window_filter);
+
+const task_sort = document.createElement('section');
+task_window.appendChild(task_sort);
+const task_sort_date = document.createElement('button');
+task_sort_date.textContent = 'sort by date';
+task_sort.appendChild(task_sort_date);
+const task_sort_id = document.createElement('button');
+task_sort_id.textContent = 'sort by ID';
+task_sort.appendChild(task_sort_id);
+
+
+const task_filter = document.createElement('fieldset');
+task_window.appendChild(task_filter);
+const task_filter_legend = document.createElement('legend');
+task_filter_legend.textContent = 'filter by status';
+task_filter.appendChild(task_filter_legend);
+
+const task_filter_incomplete = document.createElement('div');
+task_filter.appendChild(task_filter_incomplete);
+const task_filter_incomplete_input = document.createElement('input');
+task_filter_incomplete_input.type = 'radio';
+task_filter_incomplete_input.name = 'task-filter-status';
+task_filter_incomplete_input.id = 'task-filter-status__incomplete';
+task_filter_incomplete_input.value = 1;
+task_filter_incomplete_input.checked = 'checked';
+task_filter_incomplete.appendChild(task_filter_incomplete_input);
+const task_filter_incomplete_label = document.createElement('label');
+task_filter_incomplete_label.for = 'task-filter-status__incomplete';
+task_filter_incomplete_label.textContent = 'incomplete';
+task_filter_incomplete.appendChild(task_filter_incomplete_label);
+
+const task_filter_inprogress = document.createElement('div');
+task_filter.appendChild(task_filter_inprogress);
+const task_filter_inprogress_input = document.createElement('input');
+task_filter_inprogress_input.type = 'radio';
+task_filter_inprogress_input.name = 'task-filter-status';
+task_filter_inprogress_input.id = 'task-filter-status__inprogress';
+task_filter_inprogress_input.value = 2;
+task_filter_inprogress.appendChild(task_filter_inprogress_input);
+const task_filter_inprogress_label = document.createElement('label');
+task_filter_inprogress_label.for = 'task-filter-status__inprogress';
+task_filter_inprogress_label.textContent = 'in progress';
+task_filter_inprogress.appendChild(task_filter_inprogress_label);
+
+const task_filter_complete = document.createElement('div');
+task_filter.appendChild(task_filter_complete);
+const task_filter_complete_input = document.createElement('input');
+task_filter_complete_input.type = 'radio';
+task_filter_complete_input.name = 'task-filter-status';
+task_filter_complete_input.id = 'task-filter-status__complete';
+task_filter_complete_input.value = 3;
+task_filter_complete.appendChild(task_filter_complete_input);
+const task_filter_complete_label = document.createElement('label');
+task_filter_complete_label.for = 'task-filter-status__complete';
+task_filter_complete_label.textContent = 'complete';
+task_filter_complete.appendChild(task_filter_complete_label);
+
+const task_filter_all = document.createElement('div');
+task_filter.appendChild(task_filter_all);
+const task_filter_all_input = document.createElement('input');
+task_filter_all_input.type = 'radio';
+task_filter_all_input.name = 'task-filter-status';
+task_filter_all_input.id = 'task-filter-status__all';
+task_filter_all_input.value = 4;
+task_filter_all.appendChild(task_filter_all_input);
+const task_filter_all_label = document.createElement('label');
+task_filter_all_label.for = 'task-filter-status__all';
+task_filter_all_label.textContent = 'all';
+task_filter_all.appendChild(task_filter_all_label);
+
+
+const task_form = document.createElement('form');
+task_window.appendChild(task_form);
+
+const task_form_name = document.createElement('div');
+task_form.appendChild(task_form_name);
+const task_form_name_label = document.createElement('label');
+task_form_name_label.textContent = 'name:';
+task_form_name_label.for = 'task-name';
+task_form_name.appendChild(task_form_name_label);
+const task_form_name_input = document.createElement('input');
+task_form_name_input.name = 'task-name';
+task_form_name_input.required = true;
+task_form_name.appendChild(task_form_name_input);
+
+const task_form_description = document.createElement('div');
+task_form.appendChild(task_form_description);
+const task_form_description_label = document.createElement('label');
+task_form_description_label.textContent = 'description:';
+task_form_description_label.for = 'task-description';
+task_form_description.appendChild(task_form_description_label);
+const task_form_description_input = document.createElement('textarea');
+task_form_description_input.name = 'task-description';
+task_form_description.appendChild(task_form_description_input);
+
+const task_form_date = document.createElement('div');
+task_form.appendChild(task_form_date);
+const task_form_date_label = document.createElement('label');
+task_form_date_label.textContent = 'date:';
+task_form_date_label.for = 'task-date';
+task_form_date.appendChild(task_form_date_label);
+const task_form_date_input = document.createElement('input');
+task_form_date_input.name = 'task-date';
+task_form_date_input.type = 'date';
+task_form_date_input.required = true;
+task_form_date.appendChild(task_form_date_input);
+
+const task_form_submit = document.createElement('input');
+task_form_submit.type = 'submit';
+task_form_submit.value = 'add new task';
+task_form.appendChild(task_form_submit);
+
+
+const task_list = document.createElement('ul');
+task_window.appendChild(task_list);
+
+
+function setTaskDOM(task, taskElem){
+  let task_name = document.createElement('span');
+  task_name.textContent = task.name;
+  taskElem.appendChild(task_name);
+  let task_description = document.createElement('p');
+  task_description.textContent = task.description;
+  taskElem.appendChild(task_description);
+  let task_date = document.createElement('span');
+  task_date.textContent = task.date;
+  taskElem.appendChild(task_date);
+
+  let task_edit = document.createElement('button');
+  task_edit.classList.add('task__edit-btn');
+  task_edit.id = 'task_edit-btn' + task.id;
+  taskElem.appendChild(task_edit);
+
+
+  let task_status = document.createElement('span');
+  task_status.classList.add('task__status');
+  task_status.textContent = STATUSES[task.status];
+
+  let task_status_form = document.createElement('form');
+  taskElem.appendChild(task_status_form);
+  let task_status_fieldset = document.createElement('fieldset');
+  task_status_form.appendChild(task_status_fieldset);
+
+  let task_status_legend = document.createElement('legend');
+  task_status_legend.textContent = 'status';
+  task_status_fieldset.appendChild(task_status);
+
+  let task_status_incomplete = document.createElement('div');
+  task_status.appendChild(task_status_incomplete);
+  let task_status_incomplete_input = document.createElement('input');
+  task_status_incomplete_input.type = 'radio';
+  task_status_incomplete_input.name = 'task-status';
+  task_status_incomplete_input.id = 'task-status__incomplete';
+  task_status_incomplete_input.value = 1;
+  task_status_incomplete_input.checked = 'checked';
+  task_status_incomplete.appendChild(task_status_incomplete_input);
+  let task_status_incomplete_label = document.createElement('label');
+  task_status_incomplete_label.for = 'task-status__incomplete';
+  task_status_incomplete_label.textContent = 'incomplete';
+  task_status_incomplete.appendChild(task_status_incomplete_label);
+
+  let task_status_inprogress = document.createElement('div');
+  task_status.appendChild(task_status_inprogress);
+  let task_status_inprogress_input = document.createElement('input');
+  task_status_inprogress_input.type = 'radio';
+  task_status_inprogress_input.name = 'task-status';
+  task_status_inprogress_input.id = 'task-status__inprogress';
+  task_status_inprogress_input.value = 2;
+  task_status_inprogress.appendChild(task_status_inprogress_input);
+  let task_status_inprogress_label = document.createElement('label');
+  task_status_inprogress_label.for = 'task-status__inprogress';
+  task_status_inprogress_label.textContent = 'in progress';
+  task_status_inprogress.appendChild(task_status_inprogress_label);
+
+  let task_status_complete = document.createElement('div');
+  task_status.appendChild(task_status_complete);
+  let task_status_complete_input = document.createElement('input');
+  task_status_complete_input.type = 'radio';
+  task_status_complete_input.name = 'task-status';
+  task_status_complete_input.id = 'task-status__complete';
+  task_status_complete_input.value = 3;
+  task_status_complete.appendChild(task_status_complete_input);
+  let task_status_complete_label = document.createElement('label');
+  task_status_complete_label.for = 'task-status__complete';
+  task_status_complete_label.textContent = 'complete';
+  task_status_complete.appendChild(task_status_complete_label);
+
+  switch (task.status){
+    case 1:
+      task_status_incomplete_input.checked = 'checked';
+      break;
+    case 2:
+      task_status_inprogress_input.checked = 'checked';
+      break;
+    case 3:
+      task_status_complete_input.checked = 'checked';
+      break;
+  }
+
+
+  let task_remove = document.createElement('button');
+  task_remove.classList.add('task__remove-btn');
+  task_remove.id = 'task_remove-btn' + task.id;
+  taskElem.appendChild(task_remove);
+}
+
+
+if (localStorage.getItem('tasklist')){
+	taskList = JSON.parse(localStorage.getItem('tasklist-test'));
+}
+
+updateTasks();
