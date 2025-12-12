599c454d163d3f70c7699d9f5d28e31d53707db1
plida <plidapledkova@gmail.com>
2025-12-12 23:36:58 +0000

    async improvements, timezone=auto, time periods

--- public/lab4/index.js
+++ public/lab4/index.js
@@ -1,32 +1,28 @@
+const PERIODS = {0: 'night', 1: 'morning', 2: 'afternoon', 3: 'evening'};
+const WEEKDAYS = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'};
 function round(number, precision = 1){
   return Math.round(
     number * Math.pow(10, precision)
   ) / Math.pow(10, precision);
 }
 
-// example taken from https://www.w3schools.com/html/html5_geolocation.asp
-
-async function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(placeLocation, error);
-  } else {
+var getLocation = new Promise(function(resolve, reject) {
+  if (!(navigator.geolocation)) {
+    reject();
   }
-}
+  navigator.geolocation.getCurrentPosition(function(position){
+    let entry = {latitude: position.coords.latitude, longitude: position.coords.longitude, data: '', place: 'Current location'};
+    resolve(entry);
+  }) 
+})
 
-function placeLocation(position) {
-  locations.unshift({latitude: position.coords.latitude, longitude: position.coords.longitude, data: '', place: 'Current location'});
-  populateElemLocations();
-}
-
-function error() {
-  alert('Sorry, no position available.');
-}
+//let userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 // example end
 
 // example from https://www.omi.me/blogs/api-guides/how-to-fetch-weather-data-using-open-meteo-api-in-javascript
 async function getWeather (latitude, longitude) {
-  const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+  const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&timezone=auto`;
   let days = [];
   await fetch(apiUrl)
   .then(response => response.json())
@@ -41,50 +37,55 @@ function parseWeatherData(data){
   let days = [];
   let temperatures = data.hourly.temperature_2m;
     for (let i = 0; i < 7; i++){
-      let avgTemp = 0;
-      let minTemp = 1000;
-      let maxTemp = -1000;
-      for (let j = 24 * i; j < 24 * (i + 1); j++){
-        let roundTemp = round(temperatures[i], 1);
-        avgTemp += roundTemp; 
-        if (roundTemp < minTemp){
-          minTemp = roundTemp;
+      let periods = [];
+      for (let k = 0; k < 4; k++){
+        let avgTemp = 0;
+        let minTemp = 1000;
+        let maxTemp = -1000;
+        for (let j = 24 * i + (6 * k); j < 24 * (i) + (6 * (k+1)); j++){
+          let roundTemp = round(temperatures[j], 1);
+          avgTemp += roundTemp; 
+          if (roundTemp < minTemp){
+            minTemp = roundTemp;
+          }
+          if (roundTemp > maxTemp){
+            maxTemp = roundTemp;
+          }
         }
-        if (roundTemp > maxTemp){
-          maxTemp = roundTemp;
-        }
+        avgTemp = round(avgTemp / 24, 2);
+        periods.push([avgTemp, minTemp, maxTemp]);
       }
-      avgTemp = round(avgTemp / 24, 2);
-      days.push([avgTemp, minTemp, maxTemp]);
+      days.push([data.hourly.time[i*24], periods]);
     }
   return days;
 }
 
 async function populateElemLocations(){
+  console.log('populating', locations);
   if (locations.length == 0){
     return;
   }
   let elemLocationsList = elemLocations.getElementsByTagName('ul')[0];
   elemLocationsList.textContent = '';
   for (let location of locations){
-    if (location.data == ''){
-      location.data = await getWeather(location.latitude, location.longitude)
-      elemLocationsList.append(fillElement(location));
-    }
-    else{
-      elemLocationsList.append(fillElement(location));
-    }
+    elemLocationsList.append(fillElement(location));
   }
-  localStorage.setItem('locations', JSON.stringify(locations));
 }
 
 function fillElement(location){
+  console.log('filling', location);
   let elemLocation = document.createElement('li');
-  elemLocation.textContent = location.place + ' latutude' + location.latitude + 'longitude' + location.longitude;
+  elemLocation.textContent = location.place;
   for (let i = 0; i < 7; i++){
     let dayData = location.data[i];
     let elemLocationDay = document.createElement('div');
-    elemLocationDay.textContent = 'Max: ' + dayData[2] + ', min: ' + dayData[1];
+    elemLocationDay.textContent = WEEKDAYS[new Date(dayData[0].slice(0, 10)).getDay()];
+    for (let k = 0; k < 4; k++){
+      let periodData = dayData[1][k];
+      let elemLocationDayPeriod = document.createElement('div');
+      elemLocationDayPeriod.textContent = PERIODS[k] + ' Max: ' + periodData[2] + ', min: ' + periodData[1];
+      elemLocationDay.appendChild(elemLocationDayPeriod);
+    }
     elemLocation.appendChild(elemLocationDay);
   }
   return elemLocation;
@@ -100,23 +101,35 @@ if (localStorage.getItem('locations')){
 }
 
 async function setupPage(){
-  if (locations[0].place != 'Current location'){
-    await getLocation()
-  }
-
   let todaysDate = new Date().toISOString().slice(0, 10);
 
   if (localStorage.getItem('date') != todaysDate){
     for (let location of locations){
-      console.log(location);
       location.data = '';
     }
   }
   
-  localStorage.setItem('date', todaysDate);
+  for (let location of locations){
+    if (location.data == ''){
+      location.data = await getWeather(location.latitude, location.longitude);
+    }
+  }
 
+  localStorage.setItem('date', todaysDate);
+  console.log("??");
   populateElemLocations()
+
+  localStorage.setItem('locations', JSON.stringify(locations));
 }
 
-setupPage();
+if (navigator.geolocation && locations[0].place != 'Current location'){
+  getLocation.then(
+    (entry) => {console.log("??!!!"), locations.unshift(entry), setupPage()},
+    () => {console.log("??!!!!!!!"), setupPage()}
+  )
+}
+else{
+  setupPage();
+}
+
 
